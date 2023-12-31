ae6c59047d6dccab37aa1a97098c83dc59b7023e
Abdelemjid Essaid <abdelemjidessaid@gmail.com>
2023-12-31 16:07:37 +0100

    program documentation is written

--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  email: '', // your email
-  password: '', // your password
+  email: 'your_email', // your email
+  password: 'your_password', // your password
   website: 'https://intranet.alxswe.com', // do not change this link
   projectLink: 'https://intranet.alxswe.com/projects/235', // the project link
   directory: 'simple_shell', // name of directory to save in
